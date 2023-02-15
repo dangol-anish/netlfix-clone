681a6b8437c86249a87f79d79a8b678c9b4941dc
Anish Dangol <dangol.anish001@gmail.com>
2023-02-15 19:24:37 +0545

    .

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
+  console.log(process.env);
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
